c401c5ae24f8b55619cc041a754b235b36478473
fitrah0212@gmail.com <fitrah0212@gmail.com>
2023-06-08 21:25:11 +0700

    asd

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const router = require('./routes');
 
 app.use(morgan('dev'));
+app.use(express.json());
 
+app.use(router);
 
 module.exports = app;
