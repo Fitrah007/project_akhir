a146c2706cb79ef3ec08e1be8e81bfb7e1b57f4b
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2023-06-23 10:58:54 +0700

    repair transaction for roundtrip and fix display page for search data

--- controllers/airplane.js
+++ controllers/airplane.js
@@ -3,6 +3,8 @@ const { Airplane, Airline } = require('../db/models');
 module.exports = {
     getAll: async (req, res, next) => {
         try {
+            const { page = 1, per_page = 10 } = req.query
+            const offset = (page - 1) * per_page
             const airplane = await Airplane.findAll({
                 attributes: { exclude: ['createdAt', 'updatedAt'] },
                 include: [
@@ -11,9 +13,10 @@ module.exports = {
                         as: 'airline',
                         attributes: { exclude: ['createdAt', 'updatedAt'] }
                     }
-                ]
+                ],
+                limit: per_page,
+                offset: offset
             });
-
             return res.status(200).json({
                 status: true,
                 massage: 'success',
